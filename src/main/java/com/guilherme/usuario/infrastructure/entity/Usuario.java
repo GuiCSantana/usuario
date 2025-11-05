275882824f432ee527ad31975a5492c86c4cd82c
Guilherme <guilhermecsantana0@gmail.com>
2025-11-05 16:44:51 -0300

    adcionado método que salva dados de usuário

--- src/main/java/com/guilherme/usuario/infrastructure/entity/Usuario.java
+++ src/main/java/com/guilherme/usuario/infrastructure/entity/Usuario.java
@@ -1,10 +1,7 @@
 package com.guilherme.usuario.infrastructure.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -17,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table(name = "usuario")
+@Builder
 public class Usuario implements UserDetails {
 
     @Id
@@ -49,4 +47,13 @@ public class Usuario implements UserDetails {
     public String getUsername() {
         return email;
     }
+
+    public List<Endereco> getEnderecos() {
+        return List.of();
+    }
+
+    public List<Telefone> getTelefones() {
+        return List.of();
+    }
+
 }
